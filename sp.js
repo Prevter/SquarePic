2622202e8090a630d824d3e136f03cd0082fe912
Nemesh <dayspunch@gmail.com>
2017-12-07 21:35:53 +0200

    Add core

---
+++ sp.js
@@ -0,0 +1,44 @@
+function compile(pole,pw, debug=false){
+	var arr = pole.split("/");
+	var width = arr[0];
+	var img = arr[1].split("");
+	var itr = 0;
+	var html = "";
+	if(debug){
+		alert("arr = "+arr+"\n"+"width = "+width+"\n"+"img = "+img);
+	}
+	for(var j = 0; j<img.length/width; j++){
+		html += "<tr>";
+		for(var i=0; i<width; i++){ 
+			if(img[itr]=="0"){
+				html += "<td class='wht' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="1"){
+				html += "<td class='blk' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="2"){
+				html += "<td class='gr' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="3"){
+				html += "<td class='rd' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="4"){
+				html += "<td class='ylw' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="5"){
+				html += "<td class='grn' style='padding:"+pw+"px;'></td>";
+			} 
+			else if(img[itr]=="6"){
+				html += "<td class='bl' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="7"){
+				html += "<td class='orn' style='padding:"+pw+"px;'></td>";
+			}
+			else if(img[itr]=="8"){
+				html += "<td class='pnk' style='padding:"+pw+"px;'></td>";
+			}
+			itr++;
+		}
+	}
+	return html;
+}
